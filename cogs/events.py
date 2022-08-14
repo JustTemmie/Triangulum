8d2e657e2e16ce79fbc272531c03759d68fc762a
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-14 03:01:52 +0200

    initial commid

---
+++ cogs/events.py
@@ -0,0 +1,418 @@
+import discord
+from discord.ext import commands, tasks
+from discord.errors import Forbidden
+from discord.ext.commands import (
+    CommandNotFound,
+    BadArgument,
+    MissingRequiredArgument,
+    CommandOnCooldown,
+)
+import random
+from datetime import datetime
+
+import logging
+import requests
+import re
+import json
+import os
+
+from time import time
+from math import floor
+
+IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument)
+
+fish_IDs = [918830241135353907, 885113515411669002]
+
+henwees = [
+    "henwee :)",
+    "henw",
+    "green h man my beloved <3",
+    "him",
+    "what if shenwee tho??",
+    "😳",
+    "henwee is cool and all, but fuck C",
+    "hewwow :)",
+    "himwee",
+    "smilewee",
+    ":)wee",
+    "the him",
+    "little baby man",
+    "little bitch",
+    "joker lover 182",
+    "he can draw bird :)",
+    "wee wee",
+    "oui oui",
+    "theynwee",
+    '"the xwees"',
+    "fuckin unity loser lmao imagine using a program with a scuffed cube as an icon smh my head",
+    "brib",
+    "uwu",
+    "ÆwÆ",
+    '"Being harassed is dumb people shiz" ',
+    "look at him roll :)",
+    "breast cheese 😋",
+    "the man who never eats a måltid",
+    '"cheese is just cow-breast-cheese"',
+    '"man is the best gender"',
+    "bitchass gorou simp",
+    '"feeler shit, man"',
+    '"duuuu, thinkeeeeer"',
+    "this isn't henwee related i just wanted to say hi geek :)",
+    "owo",
+    "anywho, beaver clicker",
+    "m a n",
+    "man",
+    "fis :)",
+    '"https://store.steampowered.com/app/1718240/Beaver_Clicker/" - Henwee 2022',
+    '"flushed best emoji confirmed"',
+    "henwy is just some youtuber",
+    "<a:henwee_fall:955830194902544415>",
+    "henwee spin",
+    "please i'm running out of things to put here fajsfdkans nnuwiuab lvbseru sroveronaewruocnweou fuipasn coewivop8aew oaer nuoaern voaernv oer noåerunc voewurnv"
+    "holy fuck please stop with the genshin mr hen hen",
+    "weehen",
+    "beawee:)",
+    "gorou 2",
+    "gay lil bebi fis",
+    "funny fis fredaaag",
+    "🏾",
+    "🦫",
+    "<:wishlistbeaverclickeronsteam:943915845023846401>",
+    "the crippledest man in the west",
+    "Playing Genshin Impact",
+    '"a" -hen 2022',
+    '"psfttthhh i dunnu" -hen 2022',
+    '"\⬛" -hen 2022',
+    '"who tf put the physical theorapist 5 stories up?" -hen 2022',
+    '"ok fair" -hen 2022',
+    '"yes these kids suck lmao" -hen 2022',
+    '"tell me how thaumaturgy and prestidigitation is "utility", but druidcraft is "control"" -hen 2022',
+    '"mean ):" -hen 2022',
+    '"i feel like i should mention, lillevi and korede isn\'t here (😠)" -hen 2022',
+    '"most things, except liquorice" -hen 2022',
+    '"Gonna go touch it" -hen 2022',
+    "\"well i'm touchable, but I won't be hurt\" -hen 2022",
+    '"BTW I\'m not coming to school tomorrow with a lesbian haircut" -hen 2022',
+    '"remember, everyone who doesn\'t say anything agrees, just ask Elly" -hen 2022',
+    '"js is a shithole hellscape\n(which is nothing like c# i promise)" -hen 2022',
+    '"uglies shit i\'ve seen" -hen 2022',
+    '"USA er under india" -hen 2022',
+    '"not noticed that, so I guess it\'s less noticable" - hen 2022',
+    '"i dag seiner tidlig" - not hen 2022',
+    "\“blaze can go ******* ***** ***** ********************\” - hen 2022",
+    "\“I mean walking is nice\nMiss being able to\”\n- henw2022",
+    '"I\'d fuck joker from persona 5"\n"I\'d be fucked by joker from persona 5"\n- hen 2022',
+    '"You\'re like one foot in the grave, the other in the closet" - related to hen 2022 ',
+    '"æ har ingen oppion om uteliggere" - hen 2022',
+    '"no" - hen 2022',
+    '"what" - hen 2022',
+    '"æ skippe alltid denne delen av biler 1" - hen 2022',
+    '"du kan ikke bare si [...]" - hen 2022',
+    '"genshin impact e trash" - hen 2022',
+    '"cumfused" - hen 2022',
+    '"hvor mye packer lightning mcqueen?" - hen 2022',
+    '"revert revert actually fixed collison" - hen 2022',
+    '"æ hate barn, æ hate barn" - hen 2022',
+    '"🦫" - not hen 2022',
+    '"henwee quote" - hen 2022',
+    '"æ like fugla deez nuts" - hen 2022',
+    '"time to commit mass suicide" - hen 2022',
+    '"put de som en quote" - hen 2022',
+    '"https://cdn.discordapp.com/attachments/847231965764780063/966342099043753984/unknown.png" - hen 2022',
+    '"you\'re a fucking idiot" - hen 2022',
+    # copilot lmao
+    '"i\'m not a fan of the word "fucking"" -hen 2022 - github copilot',
+    '\“I\'m not a fan of the word "utility"\” - hen 2022 - github copilot',
+    '\“I\'m not a fan of the word "control"\” - hen 2022 - github copilot',
+    '"my body is a trashcan" - hen 2022 - github copilot',
+    '"joker is a trash" - hen 2022 - github copilot',
+    '"henwee is a trash" - hen 2022 - github copilot',
+]
+
+class events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.fish_friday.start()
+        self.henwee.start()
+        self.random_reddit.start()
+    
+    @commands.Cog.listener()
+    async def on_error(self, err, *args, **kwargs):
+        await self.bot.get_channel(984577196616216616).send(f"{err}")
+        
+        if err == "on_command_error":
+            await args[0].send("Sorry, something unexpected went wrong.")
+            # raise
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, exc):
+        if any([isinstance(exc, error) for error in IGNORE_EXCEPTIONS]):
+            # await ctx.send("Sorry, I couldn't find that command")
+            pass
+
+        elif isinstance(exc, MissingRequiredArgument):
+            await ctx.send(
+                f"One or more of the required arguments are missing, perhaps the help command could help you out? `{ctx.prefix}help {ctx.command}`"
+            )
+
+        elif isinstance(exc, CommandOnCooldown):
+            await ctx.send(
+                f"That command is on cooldown. Please try again in {exc.retry_after:,.2f} seconds.",
+                delete_after=(exc.retry_after*1.05 + 0.7),
+            )
+
+        #      elif isinstance(exc.original, HTTPException):
+        #          await ctx.send("Unable to send message.")
+
+        elif hasattr(exc, "original"):
+            # raise exc  # .original
+
+            if isinstance(exc.original, Forbidden):
+                await ctx.send("I do not have the permission to do that.")
+
+            else:
+                raise exc.original
+
+        else:
+            raise exc
+    
+    
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        logging.info(f"{ctx.command.name} was successfully invoked by {ctx.author}")
+        print(f"{ctx.command.name} was successfully invoked by {ctx.author} {datetime.now().hour}:{datetime.now().minute}:{datetime.now().second}")
+    
+    # fun things
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        #print(f"{member} joined {member.guild} - ({member.id} joined {member.guild.id})")
+        if member.bot:
+            return
+        
+        guilds = [694107776015663146, 918787074801401868, 946136828916944987, 885113462378876948]
+        
+        if not member.guild.id in guilds:
+            return
+        
+        if member.guild.id == 694107776015663146:  # space
+            await self.bot.get_channel(694197942000680980).send(
+                f"Welcome, {member.mention}! Read through the <#694112817611276348>, assign yourself roles in <#925393973755908136>. And if you wish, introduce yourself in <#694192946387353680>"
+            )
+            await member.add_roles(
+                member.guild.get_role(
+                    694108297086631946
+                )  # member.guild.get_role(766054606009794580)
+            )
+
+            try:
+                await member.send(f"Welcome to ***s p a c e !*** We hope you enjoy your stay :)")
+
+            except Forbidden:
+                pass
+
+        elif member.guild.id == 918787074801401868:  # frog
+            await self.bot.get_channel(918787075434762242).send(
+                f"YOOOOO {member.mention} JUST JOINED THE FROG AGENDAAAA!!!!"
+            )
+
+        elif member.guild.id == 946136828916944987:  # constaleighton
+            await self.bot.get_channel(946146531805900811).send(
+                f"hello {member.mention}, have you wishlisted beaver clicker yet?\nhttps://store.steampowered.com/app/1718240"
+            )
+        
+        elif member.guild.id == 885113462378876948:  # maid café
+            await self.bot.get_channel(885113515411669002).send(
+                f"hello {member.mention} just joined the server!\nwonder if they've wishlisted wishlisted beaver clicker yet?\nhttps://store.steampowered.com/app/1718240"
+            )
+    
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if "--" in ctx.content:
+            return
+
+        hendex = random.randrange(0, len(henwees))
+        
+        if ctx.author.bot:
+            return
+
+        if "sus" in ctx.content.lower() and not "jesus" in ctx.content.lower():
+            msg = await ctx.reply("amogus") 
+        
+        if "amog" in ctx.content.lower() or "amongu" in ctx.content.lower() or "among us" in ctx.content.lower():
+            msg = await ctx.reply("sus")
+        
+        if "bee " in ctx.content.lower() or "bee" == ctx.content.lower():   
+            await ctx.add_reaction("<a:B_:976827254799740938>")
+            
+        
+        if random.randint(0, 75000) == 2:
+            await self.react_beaver(ctx)
+        
+        
+        if ctx.author.id == 325325337615532054:  # adino
+            if random.randint(0, 500) == 2:
+                await ctx.add_reaction("<:Robpoint:885140068296196106>")
+
+
+        listies = [
+            "dam",
+            "revaeb",
+            "bippa",
+            "biba",
+            "bev",
+            "castor",
+        ]  # if message ==
+        
+        listies2 = [
+            "dam ",
+            "bidoof",
+            "bæv",
+            "beav",
+            "bever",
+        ]  # if in the message
+        
+        if ctx.guild.id != 918787074801401868:
+            listies.append("damn")
+        
+        for x in range(0, len(listies)):
+            if ctx.content.lower() == listies[x]:
+                await self.react_beaver(ctx)
+
+        for x in range(0, len(listies2)):
+            if listies2[x] in ctx.content.lower():
+                await self.react_beaver(ctx)
+                
+
+        if "brain fuck" in ctx.content:
+            await ctx.add_reaction("🧠")
+            await ctx.add_reaction("🔫")
+        
+
+        if (
+            "henwee" in ctx.content.lower()
+            or "411536312961597440" in ctx.content
+        ):  # and ctx.author.id != self.bot.user.id:
+            if random.randint(0, 15) == 2:
+                await ctx.add_reaction("<a:henwee_fall:955830194902544415>")
+                await ctx.add_reaction("<a:henwee_fall_short:955878859197280306>")
+                if random.randrange(1, 4) == 2:  # 1/3 chance
+                    await ctx.channel.send(
+                        henwees[hendex],
+                        file=discord.File("images/processed/henwee_fall.gif"),
+                    )
+
+    @tasks.loop(seconds=10)
+    async def fish_friday(self):
+        if not self.bot.ready:
+            return  
+        
+        with open("images/video/date.json", "r") as f:
+            jsoninfo = json.load(f)
+
+        if jsoninfo == str(datetime.now().day):
+            return
+
+        # sends frog in #daily frogs
+        await self.send_reddit(1008131433669341274, "frogs")
+
+        with open("images/video/date.json", "w") as f:
+            json.dump(f"{datetime.now().day}", f)
+
+        if datetime.today().weekday() != 4:
+            return
+        
+        x = 0
+        
+        if random.randrange(0, 10) == 2:
+             x = 1
+ 
+        for ID in fish_IDs:
+            if x == 0:
+                await self.bot.get_channel(ID).send("frog friday!!!", file=discord.File("images/video/funnies/funnyfrogfriday.mp4"))
+            if x == 1:
+                await self.bot.get_channel(ID).send("fr- waiit what?", file=discord.File("images/video/funnies/fish.mp4"))
+
+    @tasks.loop(hours=1)
+    async def beaver_break(self):
+        if not self.bot.ready:
+            return
+        
+        if random.randint(0, 2000) != 2:
+            return
+    
+        for ID in fish_IDs:
+            await self.bot.get_channel(ID).send("beaver time!!!", file=discord.File("images/video/funnies/thebizzarebeaverbreak.mp4"))
+    
+    @tasks.loop(seconds=293)
+    async def henwee(self):
+        if random.randrange(1, 3000) == 2:
+            await self.bot.get_channel(919666600955760702).send(
+                "reminder to keep on henweeing :)",
+                file=discord.File("images/processed/henwee_fall.gif"),
+            )
+    
+    @tasks.loop(minutes=30)
+    async def random_reddit(self):
+        await self.send_reddit(974642338150367252, "all")
+        
+    
+    async def send_reddit(self, channel, subreddit):
+        if self.bot.is_ready():
+            try:
+                req = requests.get(
+                    f"http://reddit.com/r/{subreddit}/hot.json?limit=50",
+                    headers={"User-agent": "Chrome"},
+                )
+                json = req.json()
+                if "error" in json or json["data"]["after"] is None:
+                    await self.bot.get_channel(channel).send(f"an error occured, no r/{subreddit} for now :(")
+                    return
+
+                req_len = len(json["data"]["children"])
+                for i in range(req_len):
+                    post = json["data"]["children"][i]
+                    url = post["data"]["url"] # can be image or post link
+                    if re.match(r".*\.(jpg|png|gif)$", url):
+                        break
+                              
+                title = post["data"]["title"]
+                author = "u/" + post["data"]["author"]
+                subreddit = post["data"]["subreddit_name_prefixed"]
+                link = "https://reddit.com" + post["data"]["permalink"]
+                if "selftext" in post["data"]:
+                    text = post["data"]["selftext"]  # may not exist
+                    if len(text) >= 2000:
+                        text = text[:2000].rsplit(" ", 1)[0] + " **-Snippet-**"
+                    embed = discord.Embed(title=title, description=text, url=link)
+                else:
+                    embed = discord.Embed(title=title, url=link)
+                
+                if re.match(r".*\.(jpg|png|gif)$", url):
+                    embed.set_image(url=url)
+
+                embed.set_footer(text=f"By {author} in {subreddit}")
+                
+                await self.bot.get_channel(channel).send(embed=embed)
+            
+            except Exception as e:
+                print(f"error in send_reddit: {e}")
+    
+    async def react_beaver(self, ctx):
+        await ctx.add_reaction("<a:beav:973130744190869575>")
+        if random.randrange(0, 40) == 2 and "beaver" in ctx.content:
+            message = [
+                "DID SOMEONE JUST SAY BEAVER?",
+                "beav :)",
+                "beaver clicker my beloved <3",
+                "wishlist beaver clicker today!",
+                "have you wishlisted beaver clicker yet?",
+                "go ahead, wishlist beaver clicker already",
+            ]
+            index = random.randrange(0, len(message))
+            await ctx.channel.send(
+                f"{message[index]}\nhttps://store.steampowered.com/app/1718240/Beaver_Clicker/"
+            )
+
+async def setup(bot):
+    await bot.add_cog(events(bot))
---
+++ main.py
@@ -0,0 +1,105 @@
+from email.mime import application
+import os
+import glob
+import random
+import logging
+from time import time
+import json
+import asyncio
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+import discord
+from discord.ext import tasks, commands
+
+import topgg
+#import libraries.database as db 
+
+from dotenv import load_dotenv
+
+# Load dotenv file
+load_dotenv("keys.env")
+TOKEN = os.getenv("DISCORD")#_STABLE")
+
+# Load config file
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# Grab vars from config.json
+DEFAULT_PREFIX = config["DEFAULT_PREFIX"]
+OWNER_IDS = config["OWNER_IDS"]
+APP_ID = config["APP_ID"]
+
+# Logging
+logging.basicConfig(
+    level=logging.INFO,
+    filename=f"logs/{time()}.log",
+    filemode="w",
+    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+)
+
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(DEFAULT_PREFIX),
+            intents=discord.Intents.all(),
+            owner_ids=OWNER_IDS,
+            application_id=APP_ID
+        )
+        self.synced = False
+    
+    async def setup_hook(self):
+        if not self.synced:
+            await tree.sync()#guild = discord.Object(id = 628212961218920477))
+            print("slash commands synced!")
+            self.synced = True
+    
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+        await self.wait_until_ready()
+        print("Slash commands are now ready!")
+        
+
+        if not self.ready:
+
+            guild_count = 0
+            for guild in self.guilds:
+                print(f"- {guild.id} (name: {guild.name})")
+                guild_count += 1
+
+            # Print the bot name, number of guilds, and number of shards.
+            print(f"{self.user} is in {guild_count} guild(s).\nwith {self.shard_count} shard(s)")
+
+            # Set the bot ready to True
+            self.ready = True
+
+bot = MyBot()
+tree = bot.tree
+
+# Remove default help command
+bot.remove_command("help")
+# Set the ready status to False, so the bot knows it hasnt been initialized yet.
+bot.ready = False
+
+async def load_cogs(bot):
+    print("Loading cogs...")
+    # loads cogs
+    for filename in glob.iglob("./cogs/**", recursive=True):
+        if filename.endswith('.py'):
+            filename = filename[2:].replace("/", ".") # goes from "./cogs/economy.py" to "cogs.economy.py"
+            await bot.load_extension(f'{filename[:-3]}') # removes the ".py" from the end of the filename, to make it into cogs.economy
+    
+
+async def main():
+    async with bot:
+        #await setup(bot)
+        await load_cogs(bot)
+        await bot.start(TOKEN)
+
+asyncio.run(main())
