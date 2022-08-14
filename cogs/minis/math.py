37b35ec6e6ebc064090f7b24f92f166190c1e107
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-14 06:01:28 +0200

    math

---
+++ cogs/minis/math.py
@@ -0,0 +1,39 @@
+from discord import app_commands, Interaction
+from discord.ext import commands
+
+from math import *
+from interruptingcow import timeout
+
+replacement_table = {
+    "x": "*",
+    "^": "**"
+}
+
+allowedCharacters = "1234567890()+-*/^"
+
+class mathCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="math",
+        description="i can do math too!"
+    ) 
+    async def math_command(
+        self,
+        interaction: Interaction,
+        equation: str):
+        
+        for symbol in replacement_table:
+            equation = equation.replace(symbol, replacement_table[symbol])
+        try:
+            with timeout(5, exception=RuntimeError):
+                if set(equation).difference(set(allowedCharacters)):
+                    return await interaction.response.send_messaged(f"invalid characters used, please only use the following symbols: `{allowedCharacters}`")
+                
+                await interaction.response.send_message(f"{equation} = {eval(equation)}")
+        except Exception as e:
+            await interaction.response.send_message(f"Error: {e}")
+
+async def setup(bot):
+    await bot.add_cog(mathCommands(bot))
