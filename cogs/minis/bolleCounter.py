0cf2d62a67c833d67a39708924a511dd03a6e73a
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-11-08 12:36:55 +0100

    bolle counter :blush:

---
+++ cogs/minis/bolleCounter.py
@@ -0,0 +1,50 @@
+import discord
+from discord import Member, Embed
+from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
+import json
+
+import libraries.dataStuff as dataStuff
+
+class ecobuild(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="bolle", brief="count how many bolle's you've eaten")
+    @commands.guild_only()
+    async def bolleCoutercommand(self, ctx, bolles = 0):
+        await dataStuff.open_account(self, ctx)
+
+        userNotExist = await dataStuff.check_if_not_exist(ctx.author)
+        if userNotExist:
+            return await ctx.send(f"i could not find your inventory, they need to create an account first")
+
+        bank = await dataStuff.get_bank_data()
+        
+        if bolles == 0:
+            embed = Embed()
+            embed.title = f"{ctx.author.display_name}'s bolle addiction level"
+            embed.colour = ctx.author.colour
+
+            embed.add_field(name = "they've eaten", value = f'{bank[str(ctx.author.id)]["stats"]["bolle_eaten"]} bolle')
+            
+            await ctx.send(embed=embed)
+            return
+        
+
+        bank[str(ctx.author.id)]["stats"]["bolle_eaten"] += bolles
+
+        with open("./storage/playerData.json", "w") as f:
+            json.dump(bank, f)
+        
+        embed = Embed()
+        embed.title = f"{ctx.author.display_name} just ate {bolles} bolles"
+        embed.colour = ctx.author.colour
+
+        embed.add_field(name = "now they've eaten", value = f'{bank[str(ctx.author.id)]["stats"]["bolle_eaten"]} bolle')
+        
+        await ctx.send(embed=embed)
+        
+        
+async def setup(bot):
+    await bot.add_cog(ecobuild(bot))
